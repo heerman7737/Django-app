4ec1844ac6463b1c709868be4074086b860d3c63
heerman7737 <giang.to.7737@gmail.com>
2021-08-20 14:25:21 -0700

    add json file

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,14 +76,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'USER': 'mydatabaseuser',
-        'NAME': 'mydatabase',
-        'TEST': {
-            'NAME': 'mytestdatabase',
-        },
-        'PORT':''
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
